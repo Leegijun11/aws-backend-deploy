55a69598a551bff84ed5d807fbd06e77ba03ccf4
Lee gi jun <dlrlwms11@gmail.com>
2026-06-09 00:59:27 +0900

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,6 +23,7 @@ app = FastAPI(lifespan=lifespan)
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https:https://d1vdz1ra11di34.cloudfront.net"
 ]
 
 app.add_middleware(
